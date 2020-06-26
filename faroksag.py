2813e5ab7e36c93437569f190d0ca7e731155864
unknown <balazs101010@gmail.com>
2020-06-26 18:03:39 +0200

    adf

---
+++ faroksag.py
@@ -0,0 +1,29 @@
+n = 10
+#a = [0,2,4,6]
+a=[]
+
+for i in range(0,n+4,2):
+    a.append(i)
+
+b = len(a)
+print(a)
+
+for j in a:
+    for i in range(n-1):
+        print(' ', end='')
+    print('/', end='')
+    for i in range(j):
+        print(' ', end='')
+    print('\\')
+    n=n-1
+
+for j in reversed(a):
+    for i in range(n):
+        print(' ', end='')
+    print('\\', end='')
+    for i in range(j):
+        print(' ', end='')
+    print('/')
+    n=n+1
+  
+        
